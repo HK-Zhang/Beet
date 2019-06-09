d423752d452dbf2ca8ccada357aa435e9c55464d
yxzhk <yxzhk@hotmail.com>
2019-06-09 17:30:06 +0800

    mutiple module structure

--- FisrtApp/first-app-demo/web/src/main/java/com/beet/firstappdemo/config/RouterFunctionConfiguration.java
+++ FisrtApp/first-app-demo/web/src/main/java/com/beet/firstappdemo/config/RouterFunctionConfiguration.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import com.beet.firstappdemo.domain.User;
 import com.beet.firstappdemo.repository.UserRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RequestPredicates;
