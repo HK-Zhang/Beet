da87ba718a67a53abbccff279bfecb5259cbed35
yxzhk <yxzhk@hotmail.com>
2019-06-16 09:41:12 +0800

    enable lambda

--- RxDemo/src/main/java/com/beet/App.java
+++ RxDemo/src/main/java/com/beet/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "=== RunnableTest ===" );
+
+        Runnable r1 = new Runnable(){
+        
+            public void run() {
+                System.out.println("Hello world one!");
+            }
+        };
+
+        Runnable r2 = () -> System.out.println("Hello world two!");
+
+        r1.run();
+        r2.run();
     }
 }
