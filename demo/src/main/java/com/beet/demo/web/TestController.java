99ba0eb98aed907fb3dfc4cb92b32fa632f38bf9
zhk <yxzhk@hotmail.com>
2019-07-20 21:54:48 +0800

    autowired config

--- demo/src/main/java/com/beet/demo/web/TestController.java
+++ demo/src/main/java/com/beet/demo/web/TestController.java
@@ -1,12 +1,21 @@
 package com.beet.demo.web;
 
+import com.beet.demo.doamin.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Autowired
+    Book configbook;
+
     @GetMapping("/test")
     public String test(){
         return "Hello";
     }
+
+    @GetMapping("/config")
+    public Book book() { return configbook;}
 }
