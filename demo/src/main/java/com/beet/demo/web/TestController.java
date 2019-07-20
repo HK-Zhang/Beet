7af95c2f10a58ae8b6a0848e246d8c4bf5243ac0
zhk <yxzhk@hotmail.com>
2019-07-20 20:42:13 +0800

    reorg package structure

--- demo/src/main/java/com/beet/demo/web/TestController.java
+++ demo/src/main/java/com/beet/demo/web/TestController.java
@@ -1,10 +1,10 @@
-package com.beet.demo;
+package com.beet.demo.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class  Test{
+public class TestController {
     @GetMapping("/test")
     public String test(){
         return "Hello";
